d0c1850516884514ce2d62e7cced57c3b1d94ee4
Robert Hakobyan <robert.hakobyan111@gmail.com>
2021-12-20 11:08:07 +0400

    close: #GLX-295

--- pages/marketplace/index.vue
+++ pages/marketplace/index.vue
@@ -18,7 +18,6 @@
           {{ $t("marketplace.marketplace") }}
         </h2>
         <div class="filter-block">
-          {{ filter.ordering }}
           <FilterDropdown
             :list="filterItems"
             :return-object="false"
