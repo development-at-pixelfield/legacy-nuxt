d18f995f58907f7f854b91c812e2ae35e2a3883c
Александр Гордеев <aleksandrgordeev@Aleksandrs-MacBook-Pro.local>
2021-12-21 16:21:31 +0100

    chore: close 1.1.0

--- pages/marketplace/index.vue
+++ pages/marketplace/index.vue
@@ -162,7 +162,7 @@ export default {
         }
       });
 
-      if (!query.ordering) filter.ordering = "-created_at";
+      if (!query.ordering) filter.ordering = "-deployed_at";
 
       const nfts = await store.dispatch("nfts/getNfts", filter);
       return { nfts, filter, formOptions };
@@ -183,7 +183,7 @@ export default {
       filterHeader: {},
       formOptions: {},
       filterItems: [
-        { label: "Recently listed", value: "-created_at" },
+        { label: "Recently listed", value: "-deployed_at" },
         { label: "Price (ETH): Highest first", value: "-price_eth" },
         { label: "Price (ETH): Lowest first", value: "price_eth" },
       ],
